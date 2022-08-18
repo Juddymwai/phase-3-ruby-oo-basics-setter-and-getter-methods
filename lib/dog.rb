460c0fa8739185e2c942c9df7cc6769029f5f9a6
Judy Mwai <juddiemukami@gmail.com>
2022-08-18 14:24:37 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    def name=(name)
+        @name= name
+    end
 
+    def breed=(breed)
+        @breed= breed
+    end
+
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
 end
+
+Lorri = Dog.new
+Lorri.name= "Lorri"
+ Lorri.breed = "chihuahua"
+puts Lorri.name
+puts Lorri.breed
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    def name=(name)
+        @name = name
+    end
+    def job=(job)
+        @job = job
+    end
+
+    def name
+        @name
+    end
+    def job
+        @job
+    end
 
 end
+
+jay = Person.new
+jay.name="Judy"
+jay.job="Software Dev"
+puts jay.name
+puts jay.job
